5ca2dd0e9efabf8dc5360e03186d1b9e13da55d1
Couty SSH <93544260+CoutySSH@users.noreply.github.com>
2023-05-16 20:11:17 -0300

    ....

--- ssss/sicronizar.sh
+++ ssss/sicronizar.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
-wget https://raw.githubusercontent.com/conexaoscript/renovar/main/remove.sh
+wget https://raw.githubusercontent.com/CoutySSH/SSH-WEB/main/ssss/remove.sh
 chmod 777 remove.sh
-wget https://raw.githubusercontent.com/conexaoscript/renovar/main/AtlantusMakeAccount.sh
+wget https://raw.githubusercontent.com/CoutySSH/SSH-WEB/main/ssss/AtlantusMakeAccount.sh
 chmod 777 AtlantusMakeAccount.sh
 echo -e " \e[0;31mInstalado com Sucesso \e[0m"
